f1bbd783514a549b23805ca35eb676a15795a8e0
ins315 <bhargavi.manchem@capitalone.com>
2017-10-29 21:12:41 -0400

    test

--- ServletsReimbursement/src/main/java/utilities/ConnectionUtility.java
+++ ServletsReimbursement/src/main/java/utilities/ConnectionUtility.java
@@ -1,8 +1,11 @@
 package utilities;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,12 +16,18 @@ public class ConnectionUtility {
 	public Connection getConnection() {
 		Properties dbProps = new Properties();
 		
+		System.out.println("URL !!!" );
 		
 		
 		try {
 			Class.forName("org.postgresql.Driver");
-			dbProps.load(new FileInputStream("/Users/ins315/Documents/workspace/BootCampWeek1/ReimbursementTool/src/main/resources/database.properties"));
+			//dbProps.load(new FileInputStream("/Users/ins315/Documents/workspace/BootCampWeek1/ReimbursementTool/src/main/resources/database.properties"));
+			
+			URL path = ConnectionUtility.class.getResource("database.properties");
+			File f = new File(path.getFile());
+			dbProps.load(new FileInputStream(f));
 
+			System.out.println("URL !!!" +DriverManager.getConnection(dbProps.getProperty("url") ));
 			return DriverManager.getConnection(dbProps.getProperty("url"), dbProps.getProperty("username"),
 					dbProps.getProperty("password"));
 		} catch (FileNotFoundException e) {
