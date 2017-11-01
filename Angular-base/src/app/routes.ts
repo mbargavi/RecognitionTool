97868d941fc16d33e25e928cd238b548cfc3835d
ins315 <bhargavi.manchem@capitalone.com>
2017-11-01 15:53:10 -0400

    adding new components

---
+++ Angular-base/src/app/routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HistoryComponent} from './history/history.component';
+import {MainComponent} from './main/main.component';
+import {RedemptionComponent} from './redemption/redemption.component';
+import {SearchComponent} from './search/search.component';
+
+
+export const appRoutes: Routes = [
+    {
+      path: 'login',
+      component: LoginComponent
+    },
+    {
+      path: 'history',
+      component: HistoryComponent
+    },
+    {
+      path: 'main',
+      component: MainComponent
+    },
+    {
+      path: 'redeem',
+      component: RedemptionComponent
+    },
+    {
+      path: 'search',
+      component: SearchComponent
+    },
+    // {
+    //   path: '',
+    //   redirectTo: '/history',
+    //   pathMatch: 'full'
+    // },
+    // { path: '**', component: HistoryComponent}
+];
