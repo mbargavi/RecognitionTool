a1c6384cc5fa9c138ab23b5b27c4c4d4434a58bb
ins315 <bhargavi.manchem@capitalone.com>
2017-11-03 15:20:55 -0400

    login Server code

--- src/main/java/com/capital/one/controller/EmployeeController.java
+++ src/main/java/com/capital/one/controller/EmployeeController.java
@@ -1,17 +1,21 @@
 package com.capital.one.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+import com.capital.one.datamodelbeans.Employee;
 import com.capital.one.service.EmployeeService;
+import com.capital.one.service.LoginService;
 
 
 
@@ -34,4 +38,16 @@ public class EmployeeController {
 		return myList;
 	}
 	
+	  @RequestMapping(value="/login/", method=RequestMethod.GET)
+	    public @ResponseBody ResponseEntity<Object> getUsernamepassword(@RequestParam String username,@RequestParam String password){
+		System.out.println("here!!");
+		  ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");		
+		  Employee employee = (Employee) context.getBean("employee");
+		  LoginService loginService = (LoginService)context.getBean("loginService");
+		  employee.setUserName(username);
+		  employee.setPassword(password);
+	       
+	        employee = loginService.authenticateUser(employee.getUserName(), employee.getPassword());
+			return ResponseEntity.ok(employee);
+	    }	  
 }
